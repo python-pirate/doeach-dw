ae88c027c1ea4cef77ca8f74623252ddfc8c2dad
doeach-dw <doeach.du@qq.com>
2020-01-13 22:52:25 +0800

    selection sort
    Resolve #2

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 import random
 
 def max_min(arr):
-    
     j = 1
     max = 0
     while j < len(arr):
@@ -9,3 +8,19 @@ def max_min(arr):
             max = j
         j += 1
     return arr[max]
+
+
+def select_sort(arr):
+    i = len(arr)
+    while i > 1:
+        j = 1
+        max = 0
+        while j < i:
+            if arr[j] > arr[max]:
+                max = j
+            j += 1
+
+        if max != i - 1:
+            arr[max], arr[i - 1] = arr[i - 1], arr[max]
+        
+        i -= 1
