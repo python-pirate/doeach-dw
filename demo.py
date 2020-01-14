2c369375d25963dcfcdb39749709d8192ec21950
doeach-dw <doeach.du@qq.com>
2020-01-14 10:43:27 +0800

    max/min value
    Resolve #
    
    Signed-off-by: doeach-dw <doeach.du@qq.com>

--- demo.py
+++ demo.py
@@ -9,18 +9,3 @@ def max_min(arr):
         j += 1
     return arr[max]
 
-
-def select_sort(arr):
-    i = len(arr)
-    while i > 1:
-        j = 1
-        max = 0
-        while j < i:
-            if arr[j] > arr[max]:
-                max = j
-            j += 1
-
-        if max != i - 1:
-            arr[max], arr[i - 1] = arr[i - 1], arr[max]
-        
-        i -= 1
--- test_max_min.py
+++ test_max_min.py
@@ -11,14 +11,5 @@ def test_max_min():
         for x in arr:
             assert(max >= x)
         
-def test_select_sort():
-    for _ in range(100000):
-        j = 0
-        arr = [random.randint(10, 99) for _ in range(10)]
-        select_sort(arr)
-        
-        while j < len(arr) - 1:
-            assert(arr[j] <= arr[j + 1])
-            j += 1
 
 
